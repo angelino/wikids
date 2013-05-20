b5dc483777e36bc3e874bfe96c01f756ae6102fd
Lucas Angelino <lucas.angelino@gmail.com>
2013-05-20 02:02:04 -0300

    prototipo da tela principal

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-var comicsApp = angular.module('comicsApp', ['ngResource']);
+var wikidsApp = angular.module('wikidsApp', []);
 
-comicsApp.controller('ItemsCtrl', [$scope, 'Item', function ($scope, Item) {
-  $scope.orderProp = "published_at";
-  $scope.items = Item.query();
-}]);
+wikidsApp.controller('BooksCtrl', function ($scope) {
+  $scope.orderProp = "title";
+  // $scope.items = Item.query();
+  $scope.books = allBooks();
+});
 
-commicsApp.factory('Item', [$resource, function ($resource) {
-  return $resource('/items');
-}]);
+// wikidsApp.factory('Book', [$resource, function ($resource) {
+//   return $resource('/books');
+// }]);
 
-function allItens() {
+function allBooks() {
   return [
-    {"id": "1", "description": "asdf", "editora": "marvel", "cust": "R$12,00", "imageUrl": "images/imagem1.jpg"},
-    {"id": "2", "description": "fda", "editora": "dc", "cust": "R$12,00", "imageUrl": "images/imagem2.jpg"},
-    {"id": "3", "description": " asdf asdf ", "editora": "marvel", "cust": "R$12,00", "imageUrl": "images/imagem3.jpg"},
-    {"id": "4", "description": "aasdf ", "editora": "dc", "cust": "R$12,00", "imageUrl": "images/imagem4.jpg"},
-    {"id": "5", "description": "asdf ", "editora": "punch", "cust": "R$12,00", "imageUrl": "images/imagem5.jpg"},
-    {"id": "6", "description": "fdffddfdfd fdfdf", "editora": "abril", "cust": "R$12,00", "imageUrl": "images/imagem6.jpg"},
-    {"id": "7", "description": "asdf fadf asdf", "editora": "abor", "cust": "R$12,00", "imageUrl": "images/imagem7.jpg"}
+    {"id": "1", "title": "asdf", "imageUrl": "images/chapeuzinho.png"},
+    {"id": "2", "title": "asdf", "imageUrl": "images/fabula.png"},
+    {"id": "3", "title": "asdf", "imageUrl": "images/fabula1.png"},
+    {"id": "4", "title": "asdf", "imageUrl": "images/gansa.png"},
+    {"id": "5", "title": "asdf", "imageUrl": "images/raposa.png"}
   ];
 }
