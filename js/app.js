3997fa4eb4c7654ff5b492144a48e2ce3d7ad07c
Lucas Angelino <lucas.angelino@gmail.com>
2013-06-13 00:23:43 -0300

    merge alteracoes Jimmy

--- js/app.js
+++ js/app.js
@@ -12,10 +12,23 @@ wikidsApp.controller('BooksCtrl', function ($scope) {
 
 function allBooks() {
   return [
-    {"id": "1", "title": "asdf", "imageUrl": "images/chapeuzinho.png"},
-    {"id": "2", "title": "asdf", "imageUrl": "images/fabula.png"},
-    {"id": "3", "title": "asdf", "imageUrl": "images/fabula1.png"},
-    {"id": "4", "title": "asdf", "imageUrl": "images/gansa.png"},
-    {"id": "5", "title": "asdf", "imageUrl": "images/raposa.png"}
+    {"id": "1",  "title": "asdf", "imageUrl": "images/chapeuzinho.png", "livroUrl": "book.html"},
+    {"id": "2",  "title": "asdf", "imageUrl": "images/fabula.png",      "livroUrl": "book.html"},
+    {"id": "3",  "title": "asdf", "imageUrl": "images/fabula1.png",     "livroUrl": "book.html"},
+    {"id": "4",  "title": "asdf", "imageUrl": "images/gansa.png",       "livroUrl": "book.html"},
+    {"id": "5",  "title": "asdf", "imageUrl": "images/raposa.png",      "livroUrl": "book.html"},
+
+    {"id": "6",  "title": "asdf", "imageUrl": "images/chapeuzinho.png", "livroUrl": "book.html"},
+    {"id": "7",  "title": "asdf", "imageUrl": "images/fabula.png",      "livroUrl": "book.html"},
+    {"id": "8",  "title": "asdf", "imageUrl": "images/fabula1.png",     "livroUrl": "book.html"},
+    {"id": "9",  "title": "asdf", "imageUrl": "images/gansa.png",       "livroUrl": "book.html"},
+    {"id": "10", "title": "asdf", "imageUrl": "images/raposa.png",      "livroUrl": "book.html"},
+
+    {"id": "11", "title": "asdf", "imageUrl": "images/chapeuzinho.png", "livroUrl": "book.html"},
+    {"id": "12", "title": "asdf", "imageUrl": "images/fabula.png",      "livroUrl": "book.html"},
+    {"id": "13", "title": "asdf", "imageUrl": "images/fabula1.png",     "livroUrl": "book.html"},
+    {"id": "14", "title": "asdf", "imageUrl": "images/gansa.png",       "livroUrl": "book.html"},
+    {"id": "15", "title": "asdf", "imageUrl": "images/raposa.png",      "livroUrl": "book.html"},
+    {"id": "16", "title": "asdf", "imageUrl": "images/chapeuzinho.png", "livroUrl": "book.html"}
   ];
 }
